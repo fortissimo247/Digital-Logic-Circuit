d2ce7101be18d04c9f09d4ce22758a5c9644b7aa
fortissimo247 <125655800+fortissimo247@users.noreply.github.com>
2024-05-21 12:36:58 +0900

    Update cpu_tb.v

--- first_cpu/cpu_tb.v
+++ first_cpu/cpu_tb.v
@@ -3,21 +3,13 @@
 module cpu_tb;
 
 reg clk, reset;
-wire [7:0] inst;
-wire [1:0] op, src1_addr, src2_addr, dest_addr;
-wire [7:0] src1_value, src2_value, result;
+wire [7:0] inst_out, result_out;
 
 cpu dut(
     .clk(clk),
     .reset(reset),
-    .inst(inst),
-    .op(op),
-    .src1_addr(src1_addr),
-    .src2_addr(src2_addr),
-    .dest_addr(dest_addr),
-    .src1_value(src1_value),
-    .src2_value(src2_value),
-    .result(result)
+    .inst_out(inst_out),
+    .result_out(result_out)
 );
 
 initial begin
