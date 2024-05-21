1ae33edb02c35452c1f9c92d66a5cad4d303d489
fortissimo247 <125655800+fortissimo247@users.noreply.github.com>
2024-05-21 12:25:56 +0900

    Update cpu_tb.v

--- first_cpu/cpu_tb.v
+++ first_cpu/cpu_tb.v
@@ -20,7 +20,7 @@ always #5 clk = ~clk;
 initial begin
     $dumpfile("cpu.vcd");
     $dumpvars(0, cpu_tb);
-    $dumpvars(0, dut.inst_mem.mem);
+    $dumpvars(0, dut.fetch_unit.mem);
     $dumpvars(0, dut.decode_unit.op, dut.decode_unit.src1_addr, dut.decode_unit.src2_addr, dut.decode_unit.dest_addr);
     $dumpvars(0, dut.src1_mem.memory_array, dut.src2_mem.memory_array, dut.result_mem.mem);
     $dumpvars(0, dut.execute_unit.result);
