a4751d0c37160f3749b2073c306e04ae160f7f6d
fortissimo247 <125655800+fortissimo247@users.noreply.github.com>
2024-05-21 12:31:13 +0900

    Update cpu_tb.v

--- first_cpu/cpu_tb.v
+++ first_cpu/cpu_tb.v
@@ -3,28 +3,31 @@
 module cpu_tb;
 
 reg clk, reset;
+wire [7:0] inst;
+wire [1:0] op, src1_addr, src2_addr, dest_addr;
+wire [7:0] src1_value, src2_value, result;
 
 cpu dut(
     .clk(clk),
-    .reset(reset)
+    .reset(reset),
+    .inst(inst),
+    .op(op),
+    .src1_addr(src1_addr),
+    .src2_addr(src2_addr),
+    .dest_addr(dest_addr),
+    .src1_value(src1_value),
+    .src2_value(src2_value),
+    .result(result)
 );
 
 initial begin
     clk = 0;
     reset = 1;
-    #5 reset = 0;
+    #10 reset = 0;
+    #10 reset = 1;
+    #100 $finish;
 end
 
-always #5 clk = ~clk;
-
-initial begin
-    $dumpfile("cpu.vcd");
-    $dumpvars(0, cpu_tb);
-    $dumpvars(0, dut.fetch_unit.mem);
-    $dumpvars(0, dut.decode_unit.op, dut.decode_unit.src1_addr, dut.decode_unit.src2_addr, dut.decode_unit.dest_addr);
-    $dumpvars(0, dut.src1_mem.memory_array, dut.src2_mem.memory_array, dut.result_mem.mem);
-    $dumpvars(0, dut.execute_unit.result);
-    #500 $finish;
-end
+always #50 clk = ~clk;
 
 endmodule
