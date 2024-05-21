1cc73f15d24ea90f8d9e2e10228ceb8657904a14
fortissimo247 <125655800+fortissimo247@users.noreply.github.com>
2024-05-21 12:08:57 +0900

    Update decode.v

--- first_cpu/decode.v
+++ first_cpu/decode.v
@@ -1,9 +1,14 @@
-module decode(instruction, op_code, src_addr1, src_addr2, dest_addr);
-    input [7:0] instruction;
-    output [1:0] op_code, src_addr1, src_addr2, dest_addr;
+module decode(
+    input [7:0] inst,
+    output [1:0] op,
+    output [1:0] src1_addr,
+    output [1:0] src2_addr,
+    output [1:0] dest_addr
+);
 
-    assign op_code = instruction[7:6];
-    assign src_addr1 = instruction[5:4];
-    assign src_addr2 = instruction[3:2];
-    assign dest_addr = instruction[1:0];
+assign op = inst[7:6];
+assign src1_addr = inst[5:4];
+assign src2_addr = inst[3:2];
+assign dest_addr = inst[1:0];
+
 endmodule
